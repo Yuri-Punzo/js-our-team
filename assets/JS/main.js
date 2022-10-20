6c9e9a6e8a55dde1cc45181d7244f1f46fdefcdf
Yuri-Punzo <yuripunzo@hotmail.it>
2022-10-20 14:51:16 +0200

    milestone 2

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -2,9 +2,62 @@
 Utilizzando i dati forniti,
 creare un array di oggetti per rappresentare i membri del team.
 Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
 MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+
 MILESTONE 1: Stampare su console le informazioni di nome, ruolo e la stringa della foto
+
 MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+
 BONUS 1: Trasformare la stringa foto in una immagine effettiva
+
 BONUS 2: Organizzare i singoli membri in card/schede */
+
+const ourTeam = [
+    {
+        Name: "Wayne Barnett",
+        Role: "Founder & CEO",
+        Image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        Name: "Angela Caroll",
+        Role: "Chief Editor",
+        Image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        Name: "Walter Gordon",
+        Role: "Office Manager",
+        Image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        Name: "Angela Lopez",
+        Role: "Social Media Manager",
+        Image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        Name: "Scott Estrada",
+        Role: "Developer",
+        Image: "scott-estrada-developer.jpg"
+    },
+    {
+        Name: "Barbara Ramos",
+        Role: "Graphic Designer",
+        Image: "barbara-ramos-graphic-designer.jpg"
+    }
+]
+
+//console.log(ourTeam);
+
+
+const selection = document.querySelector(".info")
+
+for (let i = 0; i < ourTeam.length; i++) {
+    const teamMember = ourTeam[i];
+    console.log(teamMember);
+    const element = document.createElement("div")
+    selection.append(element)
+    element.innerHTML = teamMember.Name + " " + teamMember.Role + " " + teamMember.Image;
+}
+
+
 
